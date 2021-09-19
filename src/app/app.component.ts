f3844e4476e5193f4922fa267e9bffcf9670d442
Maciej Tarczyski <m.tarczynski97@gmail.com>
2021-09-19 16:22:06 +0200

    create onComplete and onDelete components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,4 +19,17 @@ export class AppComponent {
     this.todo_tasks.push(todo);
     form.resetForm();
   }
+
+  onComplete(id: Guid){
+    let todo = this.todo_tasks.filter(x=>x.id == id)[0];
+    todo.isComplete = true;
+  }
+
+  onDelete(id: Guid){
+    let todo = this.todo_tasks.filter(x=>x.id == id)[0];
+    let index = this.todo_tasks.indexOf(todo, 0);
+    if(index > -1){
+      this.todo_tasks.splice(index,1); 
+    }
+  }
 }
